bff4ff337f12533c3409c9b14e9f57fa61794956
mutungapeter <petersiku99@gmail.com>
2024-07-05 17:33:25 +0300

    minor auth changes

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -23,10 +23,13 @@ userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logoutUser);
 userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me",
-   isAuthenticated,
+  //  isAuthenticated,
   getUserInfo);
 userRouter.post("/social-auth", socialAuth);
-userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-info", 
+  isAuthenticated, 
+  // updateUserInfo
+);
 userRouter.put("/update-user-password", isAuthenticated, updatePassword);
 userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
 userRouter.get(
